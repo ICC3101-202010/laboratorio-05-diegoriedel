20537bf1584fa1f5dfa59593d274e02d518a495c
diegoriedel <62268951+diegoriedel@users.noreply.github.com>
2020-04-27 22:22:48 -0400

    Creado el codigo base

---
+++ Entrega 5/Entrega 5/ChangePasswordEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entrega_5
+{
+    public class ChangePasswordEventArgs
+    {
+       
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public string Username { get; set; }
+    }
+}
